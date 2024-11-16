3b0d14f0f64d7bcdb14f81e355e06cae58627ff7
HonzikVacik <149482083+HonzikVacik@users.noreply.github.com>
2024-11-16 17:53:27 +0100

    Přeinstalace Microsoft.EntityFrameworkCore
    Přeinstalace balíčků NuGet
    Microsoft.EntityFrameworkCore
    Microsoft.EntityFrameworkCore.Design
    Microsoft.EntityFrameworkCore.Tools
    
    Pro spolupráci s Npgsql balíčkem je potřeba verze 8.0.10 těchto balíčků

--- Server/Spovyz/Spovyz/obj/Debug/net8.0/Spovyz.AssemblyInfo.cs
+++ Server/Spovyz/Spovyz/obj/Debug/net8.0/Spovyz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ea1e434f3fd04e6c410f8a6243fccb74fb2331")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55a110a88b9a165de464adc8efe4b1ef794e991e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
