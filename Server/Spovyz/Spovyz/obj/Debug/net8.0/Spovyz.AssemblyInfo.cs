520d288f38b3aa340dff5259e8e2b26c784793ec
HonzikVacik <149482083+HonzikVacik@users.noreply.github.com>
2025-01-01 15:10:28 +0100

    Přidán controller AuthController a upravena třída appsettings.json
    Přidán controller AuthController
    Ve třídě appsetings.json upraveny ip adresy issuer a audience

--- Server/Spovyz/Spovyz/obj/Debug/net8.0/Spovyz.AssemblyInfo.cs
+++ Server/Spovyz/Spovyz/obj/Debug/net8.0/Spovyz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ea9f1dc025d1051015aa25d47d1a29616c14e12")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ea390fcdf0c84f4783479492a2b919930198e948")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
