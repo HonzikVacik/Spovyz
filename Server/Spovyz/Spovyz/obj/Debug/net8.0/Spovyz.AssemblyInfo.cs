d0b17bb826c3cbe9a2853e5560a01773a8343f7a
HonzikVacik <149482083+HonzikVacik@users.noreply.github.com>
2025-01-11 15:46:18 +0100

    Employee loading
    Načítání projektů
    Načítání tasků

--- Server/Spovyz/Spovyz/obj/Debug/net8.0/Spovyz.AssemblyInfo.cs
+++ Server/Spovyz/Spovyz/obj/Debug/net8.0/Spovyz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cf7bc223f5504ececa0f1acd51c39819eb26a297")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+880f6a16f049e7253b7e4e9450134d39920a33e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
