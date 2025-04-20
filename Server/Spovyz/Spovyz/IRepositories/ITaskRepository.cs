4f1a499fb6cea4e3fa4c5b7ac03d360b3992bf08
HonzikVacik <149482083+HonzikVacik@users.noreply.github.com>
2025-04-20 17:43:44 +0200

    TaskRepository

--- Server/Spovyz/Spovyz/IRepositories/ITaskRepository.cs
+++ Server/Spovyz/Spovyz/IRepositories/ITaskRepository.cs
@@ -9,5 +9,7 @@ namespace Spovyz.IRepositories
         Task<NameBasic[]?> GetTaskNames(Project Project, uint ActiveUserId);
         Task<Models.Task?> GetTaskById(uint TaskId, uint ActiveUserId);
         System.Threading.Tasks.Task DeleteTask(Models.Task Task, Task_employee[] t_employees, Task_tag[] t_tags);
+        System.Threading.Tasks.Task PostTask(string Name, string? Description, Project Project, DateOnly? DeadLine, Enums.Status Status, Employee[] Employees, uint[] TaskIds)
+        Task<string?> PutTask(uint ActiveUserId, uint TaskId, string Name, string? Description, Project Project, DateOnly? DeadLine, Enums.Status Status, Employee[] Employees, uint[] TaskIds);
     }
 }
--- Server/Spovyz/Spovyz/Repositories/TaskRepository.cs
+++ Server/Spovyz/Spovyz/Repositories/TaskRepository.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Spovyz.IRepositories;
 using Spovyz.Models;
 using Spovyz.Transport_models;
@@ -19,6 +21,10 @@ namespace Spovyz.Repositories
             _context.RemoveRange(t_employees);
             _context.RemoveRange(t_tags);
             _context.Remove(Task);
+            _context.RemoveRange(await _context.Messages
+                .Include(m => m.Task)
+                .Where(m => m.Task == Task)
+                .ToArrayAsync());
             await _context.SaveChangesAsync();
         }
 
@@ -50,6 +56,59 @@ namespace Spovyz.Repositories
                 .Where(t => t.Task.Project == Project && t.Emlployee.Id == ActiveUserId)
                 .Select(t => new NameBasic { Id = t.Task.Id, Name = t.Task.Name })
                 .ToArrayAsync();
+        }
+
+        public async System.Threading.Tasks.Task PostTask(string Name, string? Description, Project Project, DateOnly? DeadLine, Enums.Status Status, Employee[] Employees, uint[] TaskIds)
+        {
+            Models.Task task = new Models.Task
+            {
+                Name = Name,
+                Description = Description,
+                Project = Project,
+                Dead_line = DeadLine,
+                Status = Status
+            };
+
+            var taskTags = new List<Task_tag>();
+
+            foreach (var taskId in TaskIds)
+            {
+                var tag = await _context.Tags.FirstOrDefaultAsync(tt => tt.Id == taskId);
+                if (tag != null)
+                {
+                    taskTags.Add(new Task_tag
+                    {
+                        Task = task,
+                        Tag = tag
+                    });
+                }
+            }
+
+            _context.Tasks.Add(task);
+            _context.Task_employees.AddRange(Employees.Select(e => new Task_employee { Emlployee = e, Task = task }));
+            _context.Task_tags.AddRange(taskTags);
+            await _context.SaveChangesAsync();
+        }
+
+        public async System.Threading.Tasks.Task<string?> PutTask(uint ActiveUserId, uint TaskId, string Name, string? Description, Project Project, DateOnly? DeadLine, Enums.Status Status, Employee[] Employees, uint[] TaskIds)
+        {
+            Models.Task? task = await _context.Task_employees
+                .Include(t => t.Task)
+                .Include(t => t.Emlployee)
+                .Where(t => t.Task.Id == TaskId && t.Emlployee.Id == ActiveUserId)
+                .Select(t => t.Task)
+                .FirstOrDefaultAsync();
+
+            if (task == null)
+                return "Task not found";
+
+            task.Name = Name;
+            task.Description = Description;
+            task.Project = Project;
+            task.Dead_line = DeadLine;
+            task.Status = Status;
+
+            return null;
         }
     }
 }
