fcfa07db9252c93cd237742e81540f8105ae559d
HonzikVacik <149482083+HonzikVacik@users.noreply.github.com>
2025-05-25 12:22:02 +0200

    Add CustomerController

---
+++ Server/Spovyz/Spovyz/Controllers/CustomerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Spovyz.Models;
+using Spovyz.Transport_models;
+
+namespace Spovyz.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CustomerController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> Get()
+        {
+            NameBasic[] customers = await _context.Customers
+                .Select(c => new NameBasic { Id = c.Id, Name = c.Name })
+                .ToArrayAsync();
+            return Ok(customers);
+        }
+    }
+}
