bb15fb6225a729a7c87ffe6896615e3aded39894
HonzikVacik <149482083+HonzikVacik@users.noreply.github.com>
2025-05-30 17:19:29 +0200

    Funkční navigace mezi Statement, EmployeeSalary a ActualMonth

--- Server/Spovyz/Spovyz/obj/Debug/net8.0/Spovyz.AssemblyInfo.cs
+++ Server/Spovyz/Spovyz/obj/Debug/net8.0/Spovyz.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00a7da234a7c80d8ba0bc48dade46d08aae4b350")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce8400cb249e244c980cc0e249d678cde64637ca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Spovyz")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
